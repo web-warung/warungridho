1fafb39e9f0d6386c5cb3d56e80d5d2f26b8a764
Muhammad Baihaqi Siregar <baihaqisiregar09@gmail.com>
2024-11-29 08:26:31 +0700

    respponsive

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const
+const menuIcon = document.getElementById("list");
+const menulist = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+    menulist.classList.toggle("hidden");
+});
